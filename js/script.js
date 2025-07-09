a6124a314cc85a88559d36f3fa88451be59e1320
Serg <chebest1144@gmail>
2025-07-09 14:42:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+const a = prompt('Один з останніх переглянутих фільмів?', ''),
+      b = prompt('На скільки ви його оціните?', ''),
+      c = prompt('Один з останніх переглянутих фільмів?', ''),
+      d = prompt('На скільки ви його оціните?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
